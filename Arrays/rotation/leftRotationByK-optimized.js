06315fc05b04d306b799a094e71915be43079def
JoelR <joelbiswal@gmail.com>
2026-08-04 22:46:38 +0530

    Left rotation using block-swap reverse algorithm

---
+++ Arrays/rotation/leftRotationByK-optimized.js
@@ -0,0 +1,33 @@
+// let k = Number(prompt("Enter value for k"));
+
+let k = 3;
+
+let arr = [1,2,3,4,5];
+
+k = k % arr.length;
+
+let i=0,j=arr.length-1;
+
+reverse(0,k-1);
+reverse(k,arr.length-1);
+reverse(0,arr.length-1);
+
+
+function reverse(i,j){
+
+while(i<j){
+
+     let temp = arr[i];
+     arr[i] = arr[j];
+     arr[j] = temp;
+
+     i++;
+     j--;
+}
+}
+
+
+console.log(arr);
+
+
+
