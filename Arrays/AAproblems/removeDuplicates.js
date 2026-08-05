9b2b76fc9ab90368f92a9b3f967d20d3105c038d
JoelR <joelbiswal@gmail.com>
2026-08-05 20:30:24 +0530

    Leetcode-26 solution removal of duplicate elements from sorted array

---
+++ Arrays/AAproblems/removeDuplicates.js
@@ -0,0 +1,14 @@
+let arr = [0,0,1,1,2,2,3,3];
+
+let j = 1;
+
+for(let i=0;i<arr.length-1;i++){
+
+    if(arr[i]!=arr[i+1]){
+
+        arr[j] = arr[i+1];
+        j++;
+    }
+}
+
+console.log("Number of unique elements = "+j);
