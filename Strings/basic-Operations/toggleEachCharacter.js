b06538a5a91846e82e5027e55c896725b8bc90c8
JoelR <joelbiswal@gmail.com>
2026-08-15 20:40:59 +0530

    Toggled each character in the given string

---
+++ Strings/basic-Operations/toggleEachCharacter.js
@@ -0,0 +1,20 @@
+let s = "AbCdEf";
+
+let toggle = "";
+
+for(let i=0;i<s.length;i++){
+
+    let ch = s.charCodeAt(i);
+
+    if(ch>=65 && ch<=90){
+
+        toggle += String.fromCharCode(ch+32);   // fromCharCode is opposite of charCodeAt
+    }
+
+    else if(ch>=97 && ch<=122){
+
+        toggle += String.fromCharCode(ch-32);
+    }
+}
+
+console.log(toggle);
