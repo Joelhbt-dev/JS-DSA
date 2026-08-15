58f3b0bb4b3873f83230ee5dcd1ce5dcf5e24e1a
JoelR <joelbiswal@gmail.com>
2026-08-15 19:12:44 +0530

    Printing each element of a string on new line using prebuilt charAt() method

---
+++ Strings/basic-Operations/elementsOnNewLine.js
@@ -0,0 +1,9 @@
+// Strings in javascript behave like arrays but are not arrays essentially.
+
+let s = "Coding";
+
+for(let i=0;i<s.length;i++){
+
+    console.log(s.charAt(i));   // s[i] works as well
+}
+
